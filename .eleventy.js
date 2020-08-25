50c90428170f3331f8e798685e43c13c304a11f6
Yoan Marchal <ymarchal@rnd.fr>
2020-08-25 20:59:55 +0200

    remove export & adjust conf

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ module.exports = (function(eleventyConfig) {
       input: 'src',
       includes: '_includes',
       data: '_data',
-      output: '_site',
+      output: 'dist',
     },
   };
 });
